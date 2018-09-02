5e00554fb1f4945c1f9bb5731cc06abdd65201fc
ChangedNameTo <william.m95@gmail.com>
2018-09-02 18:44:05 -0400

    Set up the helpers, file structure, and beginnings of the user model testing

--- Gemfile
+++ Gemfile
@@ -63,11 +63,12 @@ end
 
 group :test do
   gem 'rspec-rails',        '~> 3.5', '>= 3.5.2'
-  gem 'factory_girl_rails', '~> 4.7'
+  gem 'factory_bot_rails',  '~> 4.8.2'
   gem 'faker',              '~> 1.6', '>= 1.6.6'
   gem 'database_cleaner',   '~> 1.5', '>= 1.5.3'
   gem 'shoulda-matchers',   '~> 3.1'
   gem 'capybara'
+  gem 'timecop'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :integer          not null, primary key
+#  first_name       :string           not null
+#  last_name        :string           not null
+#  phone_number     :string
+#  image            :string
+#  enabled          :boolean          default(FALSE), not null
+#  email_address    :string           not null
+#  provider         :string           not null
+#  uid              :string           not null
+#  oauth_token      :string           not null
+#  oauth_expires_at :datetime         not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class User < ApplicationRecord
   # Helpers
   def full_name
