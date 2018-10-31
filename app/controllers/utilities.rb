8ab3c58d38edb634174394ae76c8042e2ade611c
Zach Edmands <zedmands@zoho.com>
2018-10-31 16:33:02 -0400

    CSV parser progress

--- app/controllers/utilities.rb
+++ app/controllers/utilities.rb
@@ -7,10 +7,15 @@ require 'csv'
 # Returns nothing.
 def parse_csv(project_data)
   CSV.foreach(project_data, quote_char: '"', col_sep: ',', row_sep: :auto, headers: true) do |row|
-    team_number = row[0]
-    project_name = row[1]
-
-
+    team_number    = row[0]
+    expo_session   = row[1]
+    table_number   = row[2]
+    project_name   = row[3]
+    project_desc   = row[4]
+    course_section = row[5]
     # TODO: create new project objects from parsed data
+    puts "#{team_number}, #{project_name}"
   end
 end
+
+parse_csv(File.expand_path("../../test/sample_csv_small.csv"))
